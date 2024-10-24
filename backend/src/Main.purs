f7ab3290118bb11c3841f9c91250d8d55600fbb0
Evie Ciobanu <me@eevie.ro>
2024-10-24 17:50:20 +0200

    add

--- backend/src/Main.purs
+++ backend/src/Main.purs
@@ -1,21 +1,28 @@
 module Main where
 
-import HTTPurple (class Generic, RouteDuplex', ServerM, mkRoute, ok, segment, serve, (/))
+import Control.Alternative ((<$>))
+import Control.Apply ((<*>))
+import Data.Int (fromString)
+import Data.Maybe (maybe)
+import HTTPurple (class Generic, RouteDuplex', ServerM, badRequest, mkRoute, ok, segment, serve, string, (/))
+import Prelude (show, ($), (+))
 import Routing.Duplex.Generic as RG
 
-import Prelude (($), (<>))
-
-data Route = Default
+data Route
+  = Home
+  | Add String String
 
 derive instance Generic Route _
 
 route :: RouteDuplex' Route
 route = mkRoute
-  { "Default": RG.noArgs
+  { "Home": RG.noArgs
+  , "Add": "add" / string segment / string segment
   }
 
 main :: ServerM
 main =
   serve { port: 8080 } { route, router }
   where
-  router { route: Default } = ok $ "hi :)"
+  router { route: Home } = ok $ "hi :)"
+  router { route: Add a b } = maybe (badRequest "") (\r -> ok $ show r) ((+) <$> fromString a <*> fromString b)
