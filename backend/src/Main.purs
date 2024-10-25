654e808b9dee8ec39a04d380da97dfcdcb6924b0
Evie Ciobanu <me@eevie.ro>
2024-10-25 13:26:37 +0200

    retry?

--- backend/src/Main.purs
+++ backend/src/Main.purs
@@ -3,9 +3,11 @@ module Main where
 import Control.Alternative ((<$>))
 import Control.Apply ((<*>))
 import Control.Bind ((>>=))
+import Data.Function ((<<<))
 import Data.Int (fromString)
 import Data.Maybe (maybe)
 import Data.Semigroup ((<>))
+import Data.String as String
 import Effect.Class (liftEffect)
 import HTTPurple (class Generic, RouteDuplex', ServerM, badRequest, mkRoute, ok, segment, serve, string, (/))
 import Node.Buffer.Class as Buffer
@@ -38,4 +40,4 @@ main =
   router { route: Add a b } = maybe (badRequest "") (\r -> ok $ show r) ((+) <$> fromString a <*> fromString b)
   router { route: Mult a b } = maybe (badRequest "") (\r -> ok $ show r) ((*) <$> fromString a <*> fromString b)
   router { route: Cowsay message } =
-    liftEffect (Process.execSync ("cowsay " <> message) >>= Buffer.toString Encoding.ASCII) >>= ok
+    liftEffect (Process.execSync ("cowsay " <> message) >>= Buffer.toString Encoding.ASCII) >>= ok <<< String.replaceAll (String.Pattern " \n") (String.Replacement "\n")
