36821fedb9917520a01c58221169f7a326e1f735
Evie Ciobanu <me@eevie.ro>
2024-10-25 13:38:37 +0200

    retry more

--- module.nix
+++ module.nix
@@ -1,8 +1,7 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
+{ config
+, lib
+, pkgs
+, ...
 }:
 let
   cfg = config.playerConfig;
@@ -50,7 +49,7 @@ in
       };
       serviceConfig = {
         DynamicUser = true;
-        ExecStartPre = "${lib.getExe pkgs.curl} -v --retry 3 --retry-delay 0 --retry-all-errors --fail -X POST ${cfg.gameServerUrl}/register/${cfg.githubLogin}/${cfg.githubRepo}";
+        ExecStartPre = "${lib.getExe pkgs.curl} -v --retry 10 --retry-delay 0 --retry-all-errors --fail -X POST ${cfg.gameServerUrl}/register/${cfg.githubLogin}/${cfg.githubRepo}";
       };
     };
 
